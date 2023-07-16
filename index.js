cb9145eee82ec320d3f41e90effe777c9b6ac4e7
Yusuf Bouzekri <yusufbouzekri@gmail.com>
2023-07-17 00:38:51 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import puppeteer from "puppeteer";
+import stealth from "puppeteer-extra-plugin-stealth";
+puppeteer.use(stealth);
+const script = async (keyword) => {
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+
+  await page.goto("https://google.com/search?q=" + keyword + "&tbm=isch");
+
+  // Set screen size
+  await page.setViewport({ width: 1080, height: 1024 });
+  await page.mouse.wheel({ deltaY: Math.random() * 1000 });
+  await page.mouse.move(Math.random(), Math.random());
+  const imageSelector = ".rg_i";
+  await page.mouse.move(Math.random(), Math.random());
+  const image = await page.waitForSelector(imageSelector);
+  const imagesrc = await image.evaluate((el) => el.src);
+  console.log(imagesrc);
+
+  await browser.close();
+};
+
+script("hi");
