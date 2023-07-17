1d80ce251f2114a4d43369b060bfbef019ce58bd
Yusuf Bouzekri <yusufbouzekri@gmail.com>
2023-07-17 09:10:33 +0100

    fix: switch import from puppeteer to puppeteer-extra

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-extra";
 import stealth from "puppeteer-extra-plugin-stealth";
 puppeteer.use(stealth);
 const script = async (keyword) => {
